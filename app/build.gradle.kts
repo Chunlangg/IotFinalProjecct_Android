9fe6e95b102703df86dd918e395f1fda1c02d47e
Lan <100866862+Chunlangg@users.noreply.github.com>
2023-10-07 21:30:05 -0400

    Running version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("com.android.application")
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -35,6 +36,9 @@ dependencies {
 
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
+    implementation("com.google.android.gms:play-services-location:17.0.0")
+    implementation("com.google.code.gson:gson:2.8.9")
+    implementation("com.google.firebase:firebase-database-ktx:20.2.2")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
--- build.gradle.kts
+++ build.gradle.kts
@@ -2,3 +2,10 @@
 plugins {
     id("com.android.application") version "8.1.2" apply false
 }
+
+buildscript {
+    dependencies {
+        classpath("com.google.gms:google-services:4.4.0")
+    }
+}
+
